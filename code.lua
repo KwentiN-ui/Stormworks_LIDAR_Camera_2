2fdc063d373aab949bd7b8470155aa55b79d7339
Quentin_Huss <quentinalexander.huss@student.hs-anhalt.de>
2023-09-12 22:14:23 +0200

    fixed upscaling

--- code.lua
+++ code.lua
@@ -1,13 +1,11 @@
 -- Laser Distance Sensor geht von -1 bis 1
-img_mult = 4 -- multiple of 2
+img_res = 32
 scanning = false
 contin = false
 curpos = 1
-zoom = 1 -- 1 -> 0
-w,h = 32,32
-img_res = h//img_mult
-posmax = img_res*img_res
-x0,y0 = 0,0
+zoom = 0.5 -- 1 -> 0
+w,h = 64,64
+x0,y0 = 0,0.5
 image = {}
 upscaled_img = {}
 function to_xy(pos,n)
@@ -53,6 +51,8 @@ function mult_upscale(img,n,k)
 	local upscaled = img
 	local nl = n
 	for i=1,k do
+		print("i:",i)
+		print(#upscaled)
 		upscaled = x2upscale(upscaled,nl)
 		nl = nl*2
 	end
@@ -82,6 +82,8 @@ function normalize(vector,minValue,maxValue)
 end
 
 function onTick()
+	posmax = img_res*img_res
+
 	-- Read data
 	dst = input.getNumber(1)
 	if input.getBool(1) then scanning=true end
@@ -90,20 +92,25 @@ function onTick()
 		laser_out = pos_to_laserc(curpos,img_res,x0,y0,zoom)
 		image[curpos] = dst
 		curpos = curpos + 1
-		print(curpos)
 		
 		if curpos-1 >= posmax then
 			scanning = false
 			curpos = 1
 			image = normalize(image,0,255)
-			upscaled_img = mult_upscale(image,img_res,img_mult)
+			scaling = math.tointeger(math.log(h^2/img_res^2)/math.log(2))
+			upscaled_img = mult_upscale(image,img_res,scaling)
+			print("Image:",(#image))
+			print("Upscaled:",(#upscaled_img))
+			print("Monitor:",h*h)
+			pause()
 		end
 		
 		output.setNumber(1,laser_out.x)
 		output.setNumber(2,laser_out.y)
 		output.setBool(1,scanning)
+
 	else
-		
+
 	end
 	
 end
@@ -116,7 +123,9 @@ function onDraw()
     		val = upscaled_img[i]
     		screen.setColor(val,val,val)
     		screen.drawRectF(px.x-1,px.y-1,1,1)
-    		
+    		screen.setColor(255,0,0)
+    		screen.drawText(2,2,#upscaled_img)
+    		screen.drawText(2,8,w*h)
     	end
     elseif image[1]==nil then
     	screen.drawText(2,2,"no\ndata")
